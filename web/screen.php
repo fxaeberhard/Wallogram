85e200764cbda0f15cdf2c8b87ae04c892343819
Francois-Xavier Aeberhard <fx@red-agent.com>
2014-04-24 09:34:07 +0200

    Added google analytics

--- web/screen.php
+++ web/screen.php
@@ -170,5 +170,22 @@ $padUrl = 'http://www.red-agent.com/wallogram/pad.php?sid=' . $screenId;
                 //Y.all(".wallogram-playpage").setStyle("height", 0);
             });
         </script>
+       
+        <!-- Google analytics -->
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-12224039-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script');
+                ga.type = 'text/javascript';
+                ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
+        
     </body>
 </html>
