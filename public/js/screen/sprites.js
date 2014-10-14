2cff8b9941d80c676210701bfe812ce0cc14ff8d
Adrien Bigler <adrienbigler@adriens-mbp.einet.ad.eivd.ch>
2014-10-14 14:50:34 +0200

    Add mannequin coloured sprites

--- public/js/screen/sprites.js
+++ public/js/screen/sprites.js
@@ -8,7 +8,32 @@
 Crafty.sprite(128, 128, 'assets/mannequin.png', {
     MannequinSprite: [0, 0]
 });
+Crafty.sprite(128, 128, 'assets/mannequin_blue.png', {
+    MannequinSpriteBlue: [0, 0]
+});
+Crafty.sprite(128, 128, 'assets/mannequin_cyan.png', {
+    MannequinSpriteCyan: [0, 0]
+});
+Crafty.sprite(128, 128, 'assets/mannequin_yellow.png', {
+    MannequinSpriteYellow: [0, 0]
+});
+Crafty.sprite(128, 128, 'assets/mannequin_green.png', {
+    MannequinSpriteGreen: [0, 0]
+});
+Crafty.sprite(128, 128, 'assets/mannequin_orange.png', {
+    MannequinSpriteOrange: [0, 0]
+});
+Crafty.sprite(128, 128, 'assets/mannequin_pink.png', {
+    MannequinSpritePink: [0, 0]
+});
+Crafty.sprite(128, 128, 'assets/mannequin_red.png', {
+    MannequinSpriteRed: [0, 0]
+});
+Crafty.sprite(128, 128, 'assets/mannequin_violet.png', {
+    MannequinSpriteViolet: [0, 0]
+});
 
 Crafty.sprite(128, 128, 'assets/wallobot-1024.png', {
     WalloBotSprite: [0, 0]
 });
+
