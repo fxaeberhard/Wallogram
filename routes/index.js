9101abfc632343606be60671395d9a894a694666
Adrien Bigler <adrienbigler@Adriens-MBP.einet.ad.eivd.ch>
2014-11-20 10:40:18 +0100

    Convert some html to jade views
    Convert pad and screen.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Wallogram'});
+});
+
+router.get('/pad', function(req, res) {
+  res.render('pad', { title: 'Wallogram - Pad', bodyClass: 'yui3-skin-sam' });
+});
+
+router.get('/screen', function(req, res) {
+  res.render('screen', { title: 'Wallogram - Pad', bodyClass: 'wallo-stdmode' });
 });
 
 module.exports = router;
