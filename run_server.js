2a6f5b6eae402baa14e6faa16cec9f32a148a141
Adrien Bigler <adrienbigler@Adriens-MBP.einet.ad.eivd.ch>
2014-11-20 08:55:13 +0100

    Setup ExpressJS structure
    Add required modules
    Add Jade view engine
    Setup routes
    Start converting html to jade

--- run_server.js
+++ run_server.js
@@ -4,13 +4,22 @@ var express = require('express');
 // Import the 'path' module (packaged with Node.js)
 var path = require('path');
 
+var routes = require('./routes/index');
+
 // Create a new instance of Express
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 
 // Import the Anagrammatix game file.
 var game = require('./server');
 
 app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/', routes);
 
 // Create a Node.js based http server on port 8080
 var server = require('http').Server(app).listen(8080, function(){
