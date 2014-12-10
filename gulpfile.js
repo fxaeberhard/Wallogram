3fe9c465fefe6be80f465d1614f70073155d9b0f
Adrien Bigler <adrienbigler@Adriens-MBP.einet.ad.eivd.ch>
2014-12-10 10:23:07 +0100

    Setup LESS configuration with Gulp (and bootstrap)

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,21 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
+var less = require('gulp-less');  
 var path = require('path');
 
 var paths = {
   styles: 'public/less/*.less'
 };
 
-gulp.task('less', function () {
-  gulp.src(paths.styles)
+gulp.task('css', function () {  
+  return gulp.src(paths.styles)
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('public/css'));
+    .pipe(gulp.dest('public/css'))
 });
 
-// Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.styles, ['less']);
+  gulp.watch(paths.styles, ['css']);
 });
 
-// The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch'])
